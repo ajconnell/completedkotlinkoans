c44772efafb47665ce38067affd5c4200c7133d0
Andy Connell <andy.connell@wackywallaby.com>
2018-07-26 13:47:19 +1000

    - Kotlin Koans -- Koan 15 --- All Any And Other Predicates

--- src/ii_collections/n14FilterMap.kt
+++ src/ii_collections/n14FilterMap.kt
@@ -10,12 +10,12 @@ fun example1(list: List<Int>) {
 
 fun Shop.getCitiesCustomersAreFrom(): Set<City> {
     // Return the set of cities the customers are from
-    return customers.map{it.city}.toSet()
+    return customers.map { it.city }.toSet()
 }
 
 fun Shop.getCustomersFrom(city: City): List<Customer> {
     // Return a list of the customers who live in the given city
-    return customers.filter{it.city == city}.toList()
+    return customers.filter { it.city == city }.toList()
 }
 
 
